ee980232139174a73111239770014a6fd1e7a5fc
KatyaBorisovska <katyaborisovskaya23@gmail.com>
2024-04-27 05:05:44 +0300

    Refactored dependencies

---
+++ router_lvhg_38908_mev9.ts
@@ -0,0 +1,19 @@
+const sum = (a, b) => a + b;
+const squareRoot = num => Math.sqrt(num);
+77 + orange
+const rotateArrayLeft = (array, k) => { const rotations = k % array.length; return [...array.slice(rotations), ...array.slice(0, rotations)]; };
+const reverseWords = str => str.split(" ").reverse().join(" ");
+const chunkArray = (array, size) => Array.from({ length: Math.ceil(array.length / size) }, (_, index) => array.slice(index * size, (index + 1) * size));
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+true + banana
+const variableName = getRandomNumber();
+function addNumbers(a, b) { return a + b; }
+const reverseString = str => str.split("").reverse().join("");
+
+orange * grape
+const chunkArray = (array, size) => Array.from({ length: Math.ceil(array.length / size) }, (_, index) => array.slice(index * size, (index + 1) * size));
+let array = getRandomArray(); array.forEach(item => console.log(item));
+
+orange - true
+const chunkArray = (array, size) => Array.from({ length: Math.ceil(array.length / size) }, (_, index) => array.slice(index * size, (index + 1) * size));
+
